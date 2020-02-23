2a63940df08a305a8072d2d233e7610ecdea6eae
Tichyus <quentin-maillard@orange.fr>
2020-02-23 15:30:19 +0100

    changement classes active + amelioration style dossier locataires

--- site/docs/4.4/examples/smartloc/scripts/scripts.js
+++ site/docs/4.4/examples/smartloc/scripts/scripts.js
@@ -7,13 +7,13 @@
 const menuNoir_items = document.querySelectorAll('.menuNoir__item')
 
 if (menuNoir_items.length > 0) {
-  menuNoir_items[0].classList.add('menuNoir__item--active')
+  menuNoir_items[0].classList.add('active')
   for (let i = 0; i < menuNoir_items.length; i++) {
     menuNoir_items[i].onclick = () => {
       for (let i = 0; i < menuNoir_items.length; i++) {
-        menuNoir_items[i].classList.remove('menuNoir__item--active')
+        menuNoir_items[i].classList.remove('active')
       }
-      menuNoir_items[i].classList.add('menuNoir__item--active')
+      menuNoir_items[i].classList.add('active')
     }
   }
 }
@@ -25,24 +25,24 @@ const menuSticky_section_items = document.querySelectorAll('.menuSticky--section
 const menuSticky_section_ancres = document.querySelectorAll('.menuSticky--section__ancre')
 const menuSticky_sousElement_ancres = document.querySelectorAll('.menuSticky--sousElement__ancre')
 
-const set_active = function (items, jumpers, className)
+const set_active = function (items, jumpers)
 {
   if (items.length > 0) {
-    items[0].classList.add(className + '--active')
+    items[0].classList.add('active')
     window.addEventListener('scroll', () => {
       for (let i = 0; i < items.length; i++) {
         const jumperPosition = jumpers[i].getBoundingClientRect().top + window.scrollY
         if (window.pageYOffset > jumperPosition - 5)
         {
           for (let j = 0; j < items.length; j++) {
-            items[j].classList.remove(className + '--active')
+            items[j].classList.remove('active')
           }
-          items[i].classList.add(className + '--active')
+          items[i].classList.add('active')
         }
       }
     })
   }
 }
 
-set_active(menuSticky_sousElement_items, menuSticky_sousElement_ancres, 'menuSticky--sousElement__item')
-set_active(menuSticky_section_items, menuSticky_section_ancres, 'menuSticky--section__item')
+set_active(menuSticky_sousElement_items, menuSticky_sousElement_ancres)
+set_active(menuSticky_section_items, menuSticky_section_ancres)
